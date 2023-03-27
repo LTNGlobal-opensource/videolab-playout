09d78f4c54ae819389e76c36a36ef6817b664d3c
Steven Toth <stoth@kernellabs.com>
2023-03-27 13:32:15 -0400

    Changes to help with 12g cards

---
+++ bin/adjust-decklink-12g-outputs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Switch the 8K Decklink card to 12g single link outputs
+./DeviceConfigure -d 0 -l 0
+./DeviceConfigure -d 1 -l 0
+./DeviceConfigure -d 2 -l 0
+./DeviceConfigure -d 3 -l 0
+
