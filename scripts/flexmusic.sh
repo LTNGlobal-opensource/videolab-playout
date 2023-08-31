423d6f4822232d3a35ab0eea8f3beed75008248a
Steven Toth <stoth@kernellabs.com>
2023-08-31 08:11:54 -0400

    Added unofficial script to srt transmit into flex

---
+++ scripts/flexmusic.sh
@@ -0,0 +1,2 @@
+./ip-generic-mpts-to-srt-push.sh --input ../streams/ch07-apollo11-ambient.ts --pcr-pid 0x100 --mcast-url udp://227.1.131.67:8000 --output-url srt://vtflex.duckdns.org:9650?streamid=video.tx/dev/demo2 --screen-port 32
+
